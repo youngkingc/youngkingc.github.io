a6b9f49aca54362f3ff54884e684e5deb3e7d323
munene_cm <91119172+youngkingc@users.noreply.github.com>
2022-03-16 10:36:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+if(this.scrollY > 20){
+    $('.navbar').addClass("sticky");
+}else{
+    $('.navbar').removeClass("sticky");
+}
+    })
+   // toggle menubar
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+
+});
+
+
+});
